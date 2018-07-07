e8cb8116a2217ed4a73fc6ed59a2627f6c202d02
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-07-06 19:20:24 -0500

    Added States and Arrays

--- Assets/Adventure.cs
+++ Assets/Adventure.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Adventure : MonoBehaviour {
+public class Adventure : MonoBehaviour
+{
+    [SerializeField] State startingState;
+    [SerializeField] Text textbox;
+
+    string[] oddDays = { "mon", "tues", "wed", "thur"};
+
+    State state;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        state = startingState;
+        textbox.text = state.GetStateStory();
+        print(oddDays[1]);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
